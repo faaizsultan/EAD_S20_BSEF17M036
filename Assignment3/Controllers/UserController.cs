7e207696f87e4b3136c84067970b1cc7e7a3a4b6
M.Faaiz S <bsef17m036@pucit.edu.pk>
2020-04-09 21:49:44 +0500

    Done with Login ANd Signup Functionality

--- Assignment3/Controllers/UserController.cs
+++ Assignment3/Controllers/UserController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using BAL;
 namespace Assignment3.Controllers
 {
     public class UserController : Controller
@@ -19,18 +19,43 @@ namespace Assignment3.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult signUp(string userTable,string Name, string password,string confirmPassword,string btnSubmit, string btnLogin)
+        public ActionResult signUp(string userTable,string userName, string password,string confirmPassword,string btnSubmit, string btnLogin)
         {
-            if (String.IsNullOrEmpty(btnLogin))
-            {
-                return View();
-            }
-            else
+            ViewData["EmptyStrings"] = "";
+            ViewData["Passwords"] = "";
+            ViewData["Successfull"] = "";
+
+            if(String.IsNullOrEmpty(btnSubmit) && (!String.IsNullOrEmpty(btnLogin))) //has clicked on Login Button
             {
                 return Redirect("~/User/login");
             }
+            
+           else if (String.IsNullOrEmpty(userName) || String.IsNullOrEmpty(confirmPassword) || String.IsNullOrEmpty(password))
+                {
+                    ViewData["EmptyStrings"] = "One of the fields is Empty";
+                    return View();
 
+                }
+           else if (password != confirmPassword)
+           {
+                    ViewData["Passwords"] = "Passwords Do not Match";
+                    return View();
+                }
+           else //has pressed Submit button to register himself..   
+           {
 
+                    if ((UserActions.registerUser(userName, password, userTable)))
+                    {
+                        ViewData["Successfull"] = "You have been successfully registered";
+                        return View();
+                    }
+                    else
+                    {
+                        ViewData["SuccessFull"] = "Already Registered";
+                        return View();
+                    }
+
+           }
         }
         
         [HttpGet]
@@ -38,12 +63,26 @@ namespace Assignment3.Controllers
         {
             return View();
         }
-        [HttpPost]s
-        public ActionResult login(string user,string password,string btnSubmit,string btnSignup)
+        [HttpPost]
+        public ActionResult login(string userName,string password,string btnSubmit,string btnSignup)
         {
-            if (String.IsNullOrEmpty(btnSignup))
+            if (String.IsNullOrEmpty(btnSignup) && !String.IsNullOrEmpty(btnSubmit)) //means hasnt clicked on signup button. && condition used for for resubission.
             {
-                return View();
+                if (String.IsNullOrEmpty(userName) || String.IsNullOrEmpty(password))
+                {
+                    ViewData["Msg"] = "One of the fields is Missing";
+                    return View();
+                }
+                else if (UserActions.isAlreadyUser(userName,password))
+                {
+                    ViewData["Msg"] = "Logged in Successfully";
+                    return View();
+                }
+                else
+                {
+                    ViewData["Msg"] = "Invalid Credentials.";
+                    return View();
+                }
             }
             else
             {
--- BAL/UserActions.cs
+++ BAL/UserActions.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using DAL;
 namespace BAL
 {
     public static class UserActions
     {
-        public static bool registerUser()
+        public static bool registerUser(string userName,string password,string userTable)
         {
-            return true;
+            if(validateUser(userName,password))
+            {
+                return UserTableOps.insertUser(userName, password, userTable);
+            }
+            return false;
             
+        }
+        public static bool validateUser(string userName,string password)
+        {
+            return UserTableOps.isValidUser(userName,password);
+        }
+        public static bool isAlreadyUser(string userName,string password)
+        {
+            return (!UserTableOps.isValidUser(userName,password));
         }
 
     }
