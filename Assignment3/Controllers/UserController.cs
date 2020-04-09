f04ffc51d6b370454e6824ac7c0d7a0a6bd04585
M.Faaiz S <bsef17m036@pucit.edu.pk>
2020-04-09 19:14:18 +0500

    Done with the Navigation between Login And signup Form

---
+++ Assignment3/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment3.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult signUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult signUp(string userTable,string Name, string password,string confirmPassword,string btnSubmit, string btnLogin)
+        {
+            if (String.IsNullOrEmpty(btnLogin))
+            {
+                return View();
+            }
+            else
+            {
+                return Redirect("~/User/login");
+            }
+
+
+        }
+        
+        [HttpGet]
+        public ActionResult login()
+        {
+            return View();
+        }
+        [HttpPost]s
+        public ActionResult login(string user,string password,string btnSubmit,string btnSignup)
+        {
+            if (String.IsNullOrEmpty(btnSignup))
+            {
+                return View();
+            }
+            else
+            {
+                return Redirect("~/User/signUp");
+            }
+        }
+
+
+
+
+    }
+}
---
+++ BAL/UserActions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL
+{
+    public static class UserActions
+    {
+        public static bool registerUser()
+        {
+            return true;
+            
+        }
+
+    }
+}
