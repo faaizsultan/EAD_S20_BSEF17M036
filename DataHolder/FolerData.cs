5e5192a6841984f7aa95b29e18406a3d7f563bdf
M.Faaiz S <bsef17m036@pucit.edu.pk>
2020-04-17 22:25:09 +0500

    Added DatHolder Class As well.

---
+++ DataHolder/FolerData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataHolder
+{
+    public class FolerData
+    {
+        public string folderName { set; get; }
+        public int ID { set; get; }
+    }
+    public class UserData
+    {
+        public string userName { set; get; }
+        public string password { set; get; }
+
+
+    }
+}
