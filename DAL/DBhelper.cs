4e66d67d25769dbdfef78b6b1ff10af228d9d350
M.Faaiz S <bsef17m036@pucit.edu.pk>
2020-04-17 23:10:26 +0500

    Added Missing DAL files And Bal Files

---
+++ DAL/DBhelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class DBHelper : IDisposable
+    {
+        //private static string conn= System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        string conn = "";
+        SqlConnection sqlcon = null;
+        public DBHelper()
+        {
+            SqlConnectionStringBuilder bu = new SqlConnectionStringBuilder();
+            bu.DataSource = @"localhost\SQLEXPRESS2016";
+            bu.InitialCatalog = "Ass3";
+            bu.UserID = "sa";
+            bu.Password = "123456789";
+            conn = bu.ToString();
+            sqlcon = new SqlConnection(conn);
+            sqlcon.Open();
+        }
+        public int returnRows(string Query)
+        {
+            SqlCommand command = new SqlCommand(Query, sqlcon);
+            var count = command.ExecuteNonQuery();
+            return count;
+
+        }
+        public Object returnObject(string Query)
+        {
+            SqlCommand command = new SqlCommand(Query, sqlcon);
+            return command.ExecuteScalar();
+        }
+        public SqlDataReader returnPointer(string Query)
+        {
+            SqlCommand command = new SqlCommand(Query, sqlcon);
+            return command.ExecuteReader();
+        }
+
+        public void Dispose()
+        {
+            if (sqlcon != null && sqlcon.State == System.Data.ConnectionState.Open)
+                sqlcon.Close();
+        }
+    }
+}
